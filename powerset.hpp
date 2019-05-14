30a8f7b6a680effdccec49f61aaed46c667b792b
sela27 <sela@goldenberg.xyz>
2019-05-14 19:08:27 +0300

    Update powerset.hpp
    empty powerset

--- powerset.hpp
+++ powerset.hpp
@@ -1,1 +1,25 @@
 #pragma once
+namespace itertools
+{
+	/*template <typename T>
+	class powerset
+	{
+	private:
+		T container;
+	public:
+		powerset(T c) : container(c) {}
+		class iterator
+		{
+
+		};
+	};*/
+
+	template <typename T>
+	class powerset
+	{
+	public:
+		powerset(const T&a) {}
+		int* begin() const { return nullptr; }
+		int* end() const { return nullptr; }
+	};
+}
